8ccbcfc4138e371b73d2d8d625d32167fc3433ad
JackR42 <j.roumimper@gmail.com>
2022-07-27 14:45:09 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {}
+}
+
 resource "azurerm_resource_group" "resourcegroup" {
   name = "S2-RG-Test42"
   location = "westeurope"
