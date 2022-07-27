28e712aa9d63594088e023241c9f924ef8ef7da3
JackR42 <j.roumimper@gmail.com>
2022-07-27 16:01:42 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,22 @@ terraform {
   backend "azurerm" {}
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "DBA42" {
   name = "S2-RG-Test42"
   location = "westeurope"
 }
+
+resource "azurerm_sql_server" "DBA42" {
+  name                         = "SQLSERVER42"
+  resource_group_name          = azurerm_resource_group.DBA42.name
+  location                     = azurerm_resource_group.DBA42.location
+  version                      = "12.0"
+  administrator_login          = "admindba"
+  administrator_login_password = "ABCabc123.42"
+}
+
+resource "azurerm_sql_database" "DBA42" {
+  name                = "DBA42"
+  resource_group_name = azurerm_resource_group.DBA42.name
+  location            = azurerm_resource_group.DBA42.location
+  server_name         = azurerm_sql_server.DBA42.name
