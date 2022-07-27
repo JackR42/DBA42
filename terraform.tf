c391bf8d61c9e6a653265768b46b36e5a41e0acc
JackR42 <j.roumimper@gmail.com>
2022-07-27 11:45:06 +0200

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  version = "latest"
+  features {}
+}
+
+terraform {
+    backend "azurerm"{
+      resource_group_name = "S2-RG-DevOps"
+      storage_account_name = "s2storageaccountdevops42"
+      container_name = "tf/terraform.tfstate"
+    }
+}
