158242e0a9abddedee122372756a95dd0aa84c00
JackR42 <j.roumimper@gmail.com>
2022-07-27 12:12:06 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,6 @@ terraform {
     backend "azurerm"{
       resource_group_name = "S2-RG-DevOps"
       storage_account_name = "s2storageaccountdevops42"
-      container_name = "tf/terraform.tfstate"
+      container_name = "terraform.tfstate"
     }
 }
